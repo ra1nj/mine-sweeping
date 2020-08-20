84b64fa033544f740a7396548c21cb26a6249f5f
rain <jiangran@blacklake.cn>
2020-08-20 11:46:59 +0800

    Initial Commit

---
+++ mine-sweeping/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mine-sweeping
+//
+//  Created by mac on 2020/8/20.
+//  Copyright © 2020 rain. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
